c2a4f2d5f2ab042fc61092f132e6b97b31a338ec
Adkhamjon <adkhamjon.sotiboldiev@student.uni-tuebingen.de>
2025-02-09 18:45:47 +0100

    add explanations about how to use .gitignore

--- Github.R
+++ Github.R
@@ -1,5 +1,41 @@
 "my git hub repo "
 
 
+# Ignore all log files
+*.log
+
+# Ignore all `.tmp` files
+*.tmp
+
+# Ignore all `.csv` files except `data.csv`
+*.csv
+!data.csv
+
+# Ignore a specific file
+secrets.txt
+
+# Ignore everything inside the "build" folder
+build/
+  
+  # Ignore all `.env` files (environment variables)
+  .env
+
+# Ignore all files starting with "temp"
+temp*
+  
+  # Ignore all `.jpg` images inside the "images" folder
+  images/*.jpg
+
+# Ignore all files named "debug" with any extension
+debug.*
+  
+  # Ignore all files in "node_modules" directory
+  node_modules/
+  
+  # Ignore all `.class` files in the "bin" directory
+  bin/*.class
+
+
+
 
 
